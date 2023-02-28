8281ae9369c64c2421096d311e5610d4be7d918e
AureLey <110115882+AureLey@users.noreply.github.com>
2023-02-28 15:45:52 +0100

    fixtures modification, rename/replace entities, User generate by make:user, JWT token done

---
+++ src/Entity/Consumer.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ConsumerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass:ConsumerRepository::class)]
+class Consumer
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $firstname = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $lastname = null;
+
+    #[ORM\ManyToOne(inversedBy: 'consumers')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+    
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -1,65 +1,157 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of Bilemo
+ *
+ * (c)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $firstname = null;
+    #[ORM\Column(length: 180, unique: true)]
+    private ?string $email = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $lastname = null;
+    #[ORM\Column]
+    private array $roles = [];
 
-    #[ORM\ManyToOne(inversedBy: 'users')]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?Client $client = null;
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[ORM\OneToMany(mappedBy: 'user', targetEntity: Consumer::class, orphanRemoval: true)]
+    private Collection $consumers;
+
+    public function __construct()
+    {
+        $this->consumers = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getFirstname(): ?string
+    public function getEmail(): ?string
     {
-        return $this->firstname;
+        return $this->email;
     }
 
-    public function setFirstname(string $firstname): self
+    public function setEmail(string $email): self
     {
-        $this->firstname = $firstname;
+        $this->email = $email;
 
         return $this;
     }
 
-    public function getLastname(): ?string
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
     {
-        return $this->lastname;
+        return (string) $this->email;
     }
 
-    public function setLastname(string $lastname): self
+    /**
+     * Méthode getUsername qui permet de retourner le champ qui est utilisé pour l'authentification.
+     */
+    public function getUsername(): string
     {
-        $this->lastname = $lastname;
+        return $this->getUserIdentifier();
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
 
         return $this;
     }
 
-    public function getClient(): ?Client
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
     {
-        return $this->client;
+        return $this->password;
     }
 
-    public function setClient(?Client $client): self
+    public function setPassword(string $password): self
     {
-        $this->client = $client;
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    /**
+     * @return Collection<int, Consumer>
+     */
+    public function getConsumers(): Collection
+    {
+        return $this->consumers;
+    }
+
+    public function addConsumer(Consumer $consumer): self
+    {
+        if (!$this->consumers->contains($consumer)) {
+            $this->consumers->add($consumer);
+            $consumer->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeConsumer(Consumer $consumer): self
+    {
+        if ($this->consumers->removeElement($consumer)) {
+            // set the owning side to null (unless already changed)
+            if ($consumer->getUser() === $this) {
+                $consumer->setUser(null);
+            }
+        }
 
         return $this;
     }
